3a66a9c463ddcc4346cf8c113f51e00f8e45d6d2
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 18:23:30 +0100

    add items Movie class

--- lib/domain/entity/movie.dart
+++ lib/domain/entity/movie.dart
@@ -1,3 +1,14 @@
 class Movie{
-  String? title;
+  final int tmdbID;
+  final String title;
+  final String posterUrl;
+  final String backdropUrl;
+  final double voteAverage;
+  final String releaseDate;
+  
+  final String overview;
+  
+  final bool isMovie;
+
+  Movie({required this.tmdbID, required this.title, required this.posterUrl, required this.backdropUrl, required this.voteAverage, required this.releaseDate, required this.overview, required this.isMovie});
 }
