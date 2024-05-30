b56bde8a9c5b219651eeb22c0886bdbe27cf88e0
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 17:11:24 +0100

    add main file and datasources, bad practice , but ... oskey

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
