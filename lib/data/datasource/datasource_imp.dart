be39414b6df70bc169334f5f630f73e520ba79ad
France! <franciscodomingosjose06798@gmail.com>
2024-05-31 00:52:21 +0100

    add dio pack, and getnowmovies implemention method

--- lib/data/datasource/datasource_imp.dart
+++ lib/data/datasource/datasource_imp.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:multiple_request_flutter/data/datasource/datasource.dart';
 import 'package:multiple_request_flutter/data/model/movie_model.dart';
 
+
+const api_key="62f37b6a721b68095d76716650d201f1";
 class DataSourceImp implements DataSource{
   @override
   Future<List<List<MovieModel>>> getMovies() {
@@ -9,9 +12,13 @@ class DataSourceImp implements DataSource{
   }
 
   @override
-  Future<List<MovieModel>> getNowPlayingMovies() {
-    // TODO: implement getNowPlayingMovies
-    throw UnimplementedError();
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get("https://api.themoviedb.org/3/movie/top_rated?api_key=$api_key");
+    if (response.statusCode == 200) {
+      throw "ok";
+    } else {
+      throw "Server Error";
+    }
   }
 
   @override
