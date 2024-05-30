684d9ed13e687fa31cdb9ba9e94cccded52eea28
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 18:32:16 +0100

    add datasource items and implementation

--- lib/data/datasource/datasource.dart
+++ lib/data/datasource/datasource.dart
@@ -1,3 +1,8 @@
+import '../model/movie_model.dart';
+
 abstract class DataSource{
-  
+  Future<List<List<MovieModel>>> getMovies(); 
+  Future<List<MovieModel>> getNowPlayingMovies();
+  Future<List<MovieModel>> getPopularMovies();
+  Future<List<MovieModel>> getTopRatedMovies();
 }
--- lib/data/datasource/datasource_imp.dart
+++ lib/data/datasource/datasource_imp.dart
@@ -1,5 +1,29 @@
 import 'package:multiple_request_flutter/data/datasource/datasource.dart';
+import 'package:multiple_request_flutter/data/model/movie_model.dart';
 
 class DataSourceImp implements DataSource{
+  @override
+  Future<List<List<MovieModel>>> getMovies() {
+    // TODO: implement getMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<MovieModel>> getNowPlayingMovies() {
+    // TODO: implement getNowPlayingMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() {
+    // TODO: implement getPopularMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<MovieModel>> getTopRatedMovies() {
+    // TODO: implement getTopRatedMovies
+    throw UnimplementedError();
+  }
   
 }
