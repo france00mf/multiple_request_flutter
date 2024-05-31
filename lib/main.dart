be602435d1098e64727beef386ec1680f6ea7cc1
France! <franciscodomingosjose06798@gmail.com>
2024-05-31 12:24:17 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(const MyWidget());
@@ -9,6 +10,8 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(body: Center(),),
+    );
   }
 }
