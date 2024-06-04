a5eb070a4f167f468a6f9e45a21e9196813a0e6d
France! <franciscodomingosjose06798@gmail.com>
2024-06-04 17:38:06 +0100

    add bloc extends for other bloc, state and event

--- lib/presenter/bloc/my_view_bloc.dart
+++ lib/presenter/bloc/my_view_bloc.dart
@@ -1,3 +1,8 @@
-class MyViewBloc extends Bloc{
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:multiple_request_flutter/presenter/bloc/my_vew_event_bloc.dart';
+import 'package:multiple_request_flutter/presenter/bloc/my_view_state_bloc.dart';
+
+class MyViewBloc extends Bloc<MyViewEventBloc, MyViewStateBloc>{
+  MyViewBloc(super.initialState);
   
 }
