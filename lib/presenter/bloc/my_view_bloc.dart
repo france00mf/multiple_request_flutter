8eb9e0a03f0072187c59f14b35d2e0a8d2b26e32
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 05:22:52 +0100

    add get_it pack# , and service locator

--- lib/presenter/bloc/my_view_bloc.dart
+++ lib/presenter/bloc/my_view_bloc.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:multiple_request_flutter/domain/use_case/movie_use_case.dart';
 import 'package:multiple_request_flutter/presenter/bloc/my_vew_event_bloc.dart';
 import 'package:multiple_request_flutter/presenter/bloc/my_view_state_bloc.dart';
 
 class MyViewBloc extends Bloc<MyViewEventBloc, MyViewStateBloc>{
-  MyViewBloc(super.initialState);
+  final MovieUseCase _getMoviesUseCase;
+  
+  MyViewBloc(this._getMoviesUseCase):super(MyViewStateBloc()){
+    on<MyViewEventBloc>((event, emit) => null);
+  }
   
 }
