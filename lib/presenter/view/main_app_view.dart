394a155ed515ad8da1f5c061f26e9738d31af173
France! <franciscodomingosjose06798@gmail.com>
2024-06-12 15:11:22 +0100

    add view for show data

---
+++ lib/presenter/view/main_app_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class MainAppView extends StatefulWidget {
+  const MainAppView({super.key});
+
+  @override
+  State<MainAppView> createState() => _MainAppViewState();
+}
+
+class _MainAppViewState extends State<MainAppView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
