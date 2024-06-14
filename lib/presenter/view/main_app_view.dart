a75748aa802d7bfa18e8e2c10fbdf565c70047c2
France! <franciscodomingosjose06798@gmail.com>
2024-06-14 15:06:03 +0100

    fix Error

--- lib/presenter/view/main_app_view.dart
+++ lib/presenter/view/main_app_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:multiple_request_flutter/core/locator/service_locator.dart';
+import 'package:multiple_request_flutter/presenter/bloc/my_vew_event_bloc.dart';
 import 'package:multiple_request_flutter/presenter/bloc/my_view_bloc.dart';
 import 'package:multiple_request_flutter/presenter/bloc/my_view_state_bloc.dart';
 
@@ -12,7 +13,7 @@ class MainAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return BlocProvider(
-      create: (context)=> locator<MyViewBloc>()..add(GetMoviesEvent()),
+      create: (context)=> locator<MyViewBloc>()..add(GetMyViewEventBloc()),
       child: Scaffold(
         body: BlocBuilder<MyViewBloc, MyViewStateBloc>(builder: (context,state){
           return throw "";
